dd3f59ca79e32f3eda8a21fda19a7bedf4ec3f61
Sebastian-Res <67005416+Sebastian-Res@users.noreply.github.com>
2025-09-06 16:04:00 +0200

    updated nvim config

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,11 +3,30 @@ return {
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
         require("lualine").setup({
+            extensions = {
+                "nvim-tree"
+            },
             options = {
-                ignore_focus = {
-                    "NvimTree"
+                theme = "gruvbox-material",
+            },
+            sections = {
+                lualine_x = {
+                    {
+                        require("noice").api.statusline.mode.get,
+                        cond = require("noice").api.statusline.mode.has,
+                    },
+                    {
+                        require("noice").api.status.command.get,
+                        cond = require("noice").api.status.command.has,
+                    },
+                    {
+                        require("noice").api.status.search.get,
+                        cond = require("noice").api.status.search.has,
+                    },
+                    'encoding',
+                    'lsp_status',
+                    'filetype',
                 },
-                theme = 'gruvbox-material'
             },
         })
     end
--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,36 +1,40 @@
 return {
     "folke/noice.nvim",
     event = "VeryLazy",
-    opts = {
-        -- add any options here
-    },
-    enabled = false,
+    enabled = true,
     dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
         "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
     },
-    config = function()
-        require("noice").setup({
-            lsp = {
-                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-                override = {
-                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                    ["vim.lsp.util.stylize_markdown"] = true,
-                    ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
-                },
+    opts = {
+        cmdline = {
+            view = "cmdline",
+        },
+        routes = {
+            { -- dont show cmdline popup when switching buffers
+                filter = { event = "cmdline", find = "bnext", },
+                opts = { skip = true },
             },
-            -- you can enable a preset for easier configuration
-            presets = {
-                bottom_search = true, -- use a classic bottom cmdline for search
-                command_palette = true, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
-                inc_rename = false, -- enables an input dialog for inc-rename.nvim
-                lsp_doc_border = false, -- add a border to hover docs and signature help
+            {
+                filter = { event = "cmdline", find = "bprevious", },
+                opts = { skip = true },
             },
-        })
-    end
+            { -- hide search count virtual text
+                filter = { event = "msg_show", kind = "search_count" },
+                opts = { skip = true },
+            },
+        },
+        lsp = {
+            progress = {
+                enabled = false
+            },
+            override = {
+                ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                ["vim.lsp.util.stylize_markdown"] = true,
+            },
+        },
+        presets = {
+            long_message_to_split = true, -- long messages will be sent to a split
+            lsp_doc_border = true,        -- add a border to hover docs and signature help
+        },
+    },
 }
