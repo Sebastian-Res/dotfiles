d9a9b8cf93483f7342278b1a7ed18e64bf8ff207
Sebastian-Res <67005416+Sebastian-Res@users.noreply.github.com>
2025-09-07 11:00:47 +0200

    fix: virtual text diagnostics now colored

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -8,6 +8,7 @@ return {
             vim.g.gruvbox_material_enable_italic = true
             vim.g.gruvbox_material_background = "hard"
             vim.g.gruvbox_material_foreground = "original"
+            vim.g.gruvbox_material_diagnostic_virtual_text = "colored"
 
             vim.cmd.colorscheme("gruvbox-material")
         end
