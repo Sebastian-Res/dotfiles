da602b64e068d3786fd4a380e8e3ce38ff5ce18e
Sebastian-Res <67005416+Sebastian-Res@users.noreply.github.com>
2026-04-02 23:42:04 +0200

    feat: added gopls and added new keymaps on attach

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++ .config/nvim/lua/plugins/mason-lspconfig.lua
@@ -8,6 +8,7 @@ return {
         require("mason-lspconfig").setup({
             ensure_installed = {
                 "lua_ls",
+                "gopls"
             },
             automatic_installation = false,
         })
--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html" },
+            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "go" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
